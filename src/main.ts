dd9654d38bec0b7aba05fa7167056d1f8ea123ee
Hossein <hosein.ghanimati1398@gmail.com>
2025-05-08 23:42:23 +0330

    handle copy

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
 
 
+const handleCopy = (e: Event) => {
+  console.log("clicked")
+  console.log(e)
+  const target = e.currentTarget as HTMLElement;
+  const text = target?.innerText || '';
+  //@ts-ignore
+  const name = e.currentTarget.dataset?.name || '';
+
+  alert(` ${name} کپی شد ✅`)
+  navigator.clipboard.writeText(text);
+}
